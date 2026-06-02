70e6d109e8c5da536ad7901fa5b94cbcde1d4607
prifield <85284068+prieceri@users.noreply.github.com>
2026-06-03 00:50:00 +0900

    버튼 애니메이션 다시만들고, 카드 프리펩 및 글꼴, 설정들 다시 바꿈

---
+++ Assets/Script/ReusableButtonAnimator.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIButtonEffect : MonoBehaviour,
+    IPointerEnterHandler,
+    IPointerExitHandler,
+    IPointerClickHandler
+{
+    [Header("Image")]
+    [SerializeField]  Image image;
+    [SerializeField]  Sprite normalSprite;
+    [SerializeField]  Sprite hoverSprite;
+
+    [Header("Scale")]
+    [SerializeField]  float hoverScale = 1.1f;
+
+    [Header("Sound")]
+    [SerializeField]  soundManager soundManager;
+    [SerializeField]  AudioClip hoverSound;
+    [SerializeField]  AudioClip clickSound;
+
+    private Vector3 originalScale;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = originalScale * hoverScale;
+
+        if (hoverSprite != null)
+            image.sprite = hoverSprite;
+
+        if (hoverSound != null)
+            soundManager.PlaySFX(hoverSound);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = originalScale;
+
+        if (normalSprite != null)
+            image.sprite = normalSprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (clickSound != null)
+            soundManager.PlaySFX(clickSound);
+    }
+}

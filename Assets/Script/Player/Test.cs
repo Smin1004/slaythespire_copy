149c3048e00bf2c0a79c065306c44d3bcad787dd
prifield <85284068+prieceri@users.noreply.github.com>
2026-06-03 21:20:50 +0900

    1

--- Assets/Script/Player/Test.cs
+++ Assets/Script/Player/Test.cs
@@ -6,7 +6,7 @@ public class Test: MonoBehaviour
     [Header("----- 컴포넌트 -----")]
     [SerializeField] DamageViewSpawner _damageSpawner;  // 대미지뷰 스포너
 
-    // Update is called once per frame
+    //111 Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))

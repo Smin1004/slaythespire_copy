70babe60e5b469d15ea52f07588e04e272c0165d
prifield <85284068+prieceri@users.noreply.github.com>
2026-06-03 20:54:21 +0900

    대미지 텍스트 만들었다

---
+++ Assets/Script/Player/Test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Test: MonoBehaviour
+{
+
+    [Header("----- 컴포넌트 -----")]
+    [SerializeField] DamageViewSpawner _damageSpawner;  // 대미지뷰 스포너
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            _damageSpawner.SpawnDamageView(Random.Range(10, 999));
+    
+    
+    }
+}
